19349fb70722a826697ed4651a3b7ba8039b0b5e
Naresh Jain <nashjain@gmail.com>
2014-04-16 08:20:53 +0530

    Complete Refactoring of the code Added sound and reset capability

--- timer.js
+++ timer.js
@@ -1,82 +1,66 @@
-(function() {
-  var formatSeconds, truncate;
-  truncate = function(x) {
-    if (x > 0) {
-      return Math.floor(x);
-    } else {
-      return Math.ceil(x);
+var timer = {
+    paused: true,
+    active_interval: null,
+    initial: null,
+    seconds: 300,
+    callbacks: {},
+    truncate: function (x) {
+        return x > 0 ? Math.floor(x) : Math.ceil(x);
+    },
+    format_seconds: function (seconds) {
+        var sign = seconds >= 0 ? "" : "-";
+        var min = Math.abs(timer.truncate(seconds / 60));
+        var sec = ("0" + Math.abs(seconds % 60)).slice(-2);
+        return "" + sign + min + ":" + sec;
+    },
+    start: function () {
+        timer.paused = false;
+        if (timer.active_interval == null)
+            timer.active_interval = setInterval(timer.tick, 1000);
+    },
+    stop: function () {
+        timer.paused = true;
+        if (timer.active_interval != null) {
+            clearInterval(timer.active_interval);
+            timer.active_interval = null;
+        }
+    },
+    pause: function () {
+        timer.paused ? timer.start() : timer.stop();
+    },
+    reset: function () {
+        timer.stop();
+        timer.seconds = timer.initial;
+        if (typeof timer.callbacks.on_tick === "function")
+            timer.callbacks.on_tick(timer.human_time());
+    },
+    tick: function () {
+        if (!timer.paused) {
+            timer.seconds -= 1;
+            if (typeof timer.callbacks.on_tick === "function")
+                timer.callbacks.on_tick(timer.human_time());
+            if (typeof timer.callbacks[timer.seconds] === "function")
+                timer.callbacks[timer.seconds]();
+        }
+    },
+    human_time: function () {
+        return timer.format_seconds(timer.seconds);
+    },
+    init: function (callbacks) {
+        timer.seconds = timer.duration();
+        timer.initial = timer.seconds;
+        if (callbacks != null) {
+            timer.callbacks = callbacks;
+        }
+    },
+    duration: function () {
+        var query = window.location.search.substring(1);
+        if (query) {
+            var pair = query.split("=");
+            if (pair[0] == 'd') {
+                return pair[1];
+            }
+        }
+        return timer.seconds;
     }
-  };
-  formatSeconds = function(seconds) {
-    var min, sec, sign;
-    sign = seconds >= 0 ? "" : "-";
-    min = Math.abs(truncate(seconds / 60));
-    sec = ("0" + Math.abs(seconds % 60)).slice(-2);
-    return "" + sign + min + ":" + sec;
-  };
-  this.Timer = (function() {
-    function Timer(seconds, callbacks) {
-      var activeInterval, initial, paused, start, stop, tick;
-      if (seconds == null) {
-        seconds = 300;
-      }
-      if (callbacks == null) {
-        callbacks = {};
-      }
-      initial = seconds;
-      paused = true;
-      activeInterval = null;
-      tick = function() {
-        if (!paused) {
-          seconds -= 1;
-          if (typeof callbacks.tick === "function") {
-            callbacks.tick(seconds, formatSeconds(seconds));
-          }
-          return typeof callbacks[seconds] === "function" ? callbacks[seconds]() : void 0;
-        }
-      };
-      this.getSeconds = function() {
-        return seconds;
-      };
-      this.getHumanTime = function() {
-        return formatSeconds(seconds);
-      };
-      this.start = function() {
-        paused = false;
-                if (activeInterval != null) {
-          activeInterval;
-        } else {
-          activeInterval = setInterval(tick, 1000);
-        };
-        return this;
-      };
-      start = this.start;
-      this.stop = function() {
-        paused = true;
-        if (activeInterval != null) {
-          clearInterval(activeInterval);
-          activeInterval = null;
-        }
-        return this;
-      };
-      stop = this.stop;
-      this.pause = function() {
-        if (paused) {
-          start();
-        } else {
-          stop();
-        }
-        return this;
-      };
-      this.reset = function() {
-        this.stop();
-        seconds = initial;
-        if (typeof callbacks.reset === "function") {
-          callbacks.reset();
-        }
-        return this;
-      };
-    }
-    return Timer;
-  })();
-}).call(this);
+};
